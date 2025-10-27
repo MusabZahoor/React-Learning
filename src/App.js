558400f561be824e2e3242179aa77efe03d47d6c
musabzahoor <musabzahoor@gmail.com>
2025-10-27 09:08:46 +0500

    Before 27th OCT Class

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./header/Header";
+import Body from "./content/Body";
+import Footer from "./footer/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/content/Body.jsx
@@ -0,0 +1,9 @@
+import "./Body.css";
+const Body = () => {
+  return (
+    <>
+      <div className="body"></div>
+    </>
+  );
+};
+export default Body;
